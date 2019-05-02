ae44182541a6eb06c051bb420f14656bd1799140
M1rceaDogaru <36267384+M1rceaDogaru@users.noreply.github.com>
2019-05-02 23:05:58 +0300

    Added spritesheets

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float AnimationSpeed = 0.2f;
+
+    private Rigidbody2D _body;
+    private Animation _runAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _runAnimation = GetComponentInChildren<Animation>();
+        _runAnimation["Run"].speed = AnimationSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+        _body.gravityScale = -_body.gravityScale;
+    }
+}
